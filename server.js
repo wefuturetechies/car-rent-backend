38eaae417664d8400a438e202d8e763bc943b318
krishna <Wefuturetechies@gmail.com>
2026-02-19 22:47:26 +0530

    push

--- server.js
+++ server.js
@@ -8,7 +8,21 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://car-rent-backend-production-fb17.up.railway.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
+
+app.options("*", cors());
+
+app.use(express.json());
+
 app.use(express.json());
 app.use('/uploads', express.static('uploads')); // Serve uploaded files
 
